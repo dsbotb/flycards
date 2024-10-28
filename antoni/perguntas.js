de48418b995ec0f88395f4f19dfebe7f03e85fd7
dsbotb <143203964+dsbotb@users.noreply.github.com>
2024-10-28 11:58:35 -0300

    Add files via upload

---
+++ antoni/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'SOU ANTONI '
+)
+criaCartao(
+    'Minha Familia',
+    '',
+    'E formada por 5 pessoas na minha casa mais e maio...'
+)
+criaCartao(
+    'ONDE MORO',
+    '',
+    'Moro na vila caldas '
+)
+
+criaCartao(
+    'Onde estudo',
+    '',
+    'Estudo no colégio Morski'
+)
+criaCartao(
+    'HOBBY',
+    '',
+    'GOSTO DE programação'
+)
+criaCartao(
+    'minha turma ',
+    '',
+    'e muito boa e e diferente de antes'
+)
+
+criaCartao(
+    'faculdade',
+    '',
+    'PREtendo fazer programação'
+)
+criaCartao(
+    'minha cidade ',
+    '',
+    'e muito boa '
+)
+criaCartao(
+    'meu sonho ',
+    '',
+    'e trabalhar em algo que goste e conquistar muito dinheiro ca minha familia'
+)
+
+criaCartao(
+    'fim',
+    '',
+    'FOI ISSO O TRABALHO DE HOJE'
+)
